eb98c7637af437fbff0a6bb503afc11e83661960
Vasu Gupta <guptavasu1213@gmail.com>
2020-04-01 23:39:34 -0600

    Admin can update Branch entries

--- CarRentalApp/FindBranch.Designer.cs
+++ CarRentalApp/FindBranch.Designer.cs
@@ -38,15 +38,13 @@
             this.branchIDSearchButton = new System.Windows.Forms.Button();
             this.topPanel = new System.Windows.Forms.Panel();
             this.findExistingVehicleLabel = new System.Windows.Forms.Label();
-            this.updateButton = new System.Windows.Forms.Button();
-            this.deleteButton = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.employeeIDGroupBox = new System.Windows.Forms.GroupBox();
-            this.userDataGridView = new System.Windows.Forms.DataGridView();
+            this.branchInfoDataGridView = new System.Windows.Forms.DataGridView();
             this.topPanel.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.employeeIDGroupBox.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.userDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.branchInfoDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // branchIDResultLabel
@@ -54,10 +52,9 @@
             this.branchIDResultLabel.AutoSize = true;
             this.branchIDResultLabel.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.branchIDResultLabel.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.branchIDResultLabel.Location = new System.Drawing.Point(22, 72);
-            this.branchIDResultLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.branchIDResultLabel.Location = new System.Drawing.Point(29, 89);
             this.branchIDResultLabel.Name = "branchIDResultLabel";
-            this.branchIDResultLabel.Size = new System.Drawing.Size(0, 19);
+            this.branchIDResultLabel.Size = new System.Drawing.Size(0, 22);
             this.branchIDResultLabel.TabIndex = 26;
             this.branchIDResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.branchIDResultLabel.Visible = false;
@@ -66,10 +63,10 @@
             // 
             this.branchIDTextbox.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.branchIDTextbox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.branchIDTextbox.Location = new System.Drawing.Point(137, 37);
-            this.branchIDTextbox.Margin = new System.Windows.Forms.Padding(2);
+            this.branchIDTextbox.Location = new System.Drawing.Point(183, 46);
+            this.branchIDTextbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.branchIDTextbox.Name = "branchIDTextbox";
-            this.branchIDTextbox.Size = new System.Drawing.Size(192, 26);
+            this.branchIDTextbox.Size = new System.Drawing.Size(255, 30);
             this.branchIDTextbox.TabIndex = 24;
             // 
             // carIDLabel
@@ -77,10 +74,9 @@
             this.carIDLabel.AutoSize = true;
             this.carIDLabel.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.carIDLabel.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.carIDLabel.Location = new System.Drawing.Point(17, 37);
-            this.carIDLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.carIDLabel.Location = new System.Drawing.Point(23, 46);
             this.carIDLabel.Name = "carIDLabel";
-            this.carIDLabel.Size = new System.Drawing.Size(69, 19);
+            this.carIDLabel.Size = new System.Drawing.Size(85, 22);
             this.carIDLabel.TabIndex = 23;
             this.carIDLabel.Text = "Branch ID";
             // 
@@ -89,10 +85,9 @@
             this.branchNameResultLabel.AutoSize = true;
             this.branchNameResultLabel.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.branchNameResultLabel.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.branchNameResultLabel.Location = new System.Drawing.Point(22, 72);
-            this.branchNameResultLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.branchNameResultLabel.Location = new System.Drawing.Point(29, 89);
             this.branchNameResultLabel.Name = "branchNameResultLabel";
-            this.branchNameResultLabel.Size = new System.Drawing.Size(0, 19);
+            this.branchNameResultLabel.Size = new System.Drawing.Size(0, 22);
             this.branchNameResultLabel.TabIndex = 26;
             this.branchNameResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.branchNameResultLabel.Visible = false;
@@ -103,10 +98,10 @@
             this.branchNameSearchButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.branchNameSearchButton.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.branchNameSearchButton.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.branchNameSearchButton.Location = new System.Drawing.Point(204, 79);
-            this.branchNameSearchButton.Margin = new System.Windows.Forms.Padding(2);
+            this.branchNameSearchButton.Location = new System.Drawing.Point(272, 97);
+            this.branchNameSearchButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.branchNameSearchButton.Name = "branchNameSearchButton";
-            this.branchNameSearchButton.Size = new System.Drawing.Size(65, 30);
+            this.branchNameSearchButton.Size = new System.Drawing.Size(87, 37);
             this.branchNameSearchButton.TabIndex = 25;
             this.branchNameSearchButton.Text = "Search";
             this.branchNameSearchButton.UseVisualStyleBackColor = false;
@@ -116,10 +111,10 @@
             // 
             this.branchNameTextBox.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.branchNameTextBox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.branchNameTextBox.Location = new System.Drawing.Point(137, 37);
-            this.branchNameTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.branchNameTextBox.Location = new System.Drawing.Point(183, 46);
+            this.branchNameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.branchNameTextBox.Name = "branchNameTextBox";
-            this.branchNameTextBox.Size = new System.Drawing.Size(192, 26);
+            this.branchNameTextBox.Size = new System.Drawing.Size(255, 30);
             this.branchNameTextBox.TabIndex = 24;
             // 
             // branchNameLabel
@@ -127,10 +122,9 @@
             this.branchNameLabel.AutoSize = true;
             this.branchNameLabel.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.branchNameLabel.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.branchNameLabel.Location = new System.Drawing.Point(17, 37);
-            this.branchNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.branchNameLabel.Location = new System.Drawing.Point(23, 46);
             this.branchNameLabel.Name = "branchNameLabel";
-            this.branchNameLabel.Size = new System.Drawing.Size(91, 19);
+            this.branchNameLabel.Size = new System.Drawing.Size(114, 22);
             this.branchNameLabel.TabIndex = 23;
             this.branchNameLabel.Text = "Branch Name";
             // 
@@ -140,10 +134,10 @@
             this.branchIDSearchButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.branchIDSearchButton.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.branchIDSearchButton.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.branchIDSearchButton.Location = new System.Drawing.Point(204, 79);
-            this.branchIDSearchButton.Margin = new System.Windows.Forms.Padding(2);
+            this.branchIDSearchButton.Location = new System.Drawing.Point(272, 97);
+            this.branchIDSearchButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.branchIDSearchButton.Name = "branchIDSearchButton";
-            this.branchIDSearchButton.Size = new System.Drawing.Size(65, 30);
+            this.branchIDSearchButton.Size = new System.Drawing.Size(87, 37);
             this.branchIDSearchButton.TabIndex = 25;
             this.branchIDSearchButton.Text = "Search";
             this.branchIDSearchButton.UseVisualStyleBackColor = false;
@@ -157,9 +151,9 @@
             this.topPanel.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.topPanel.ForeColor = System.Drawing.SystemColors.ActiveBorder;
             this.topPanel.Location = new System.Drawing.Point(0, 0);
-            this.topPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.topPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.topPanel.Name = "topPanel";
-            this.topPanel.Size = new System.Drawing.Size(1084, 69);
+            this.topPanel.Size = new System.Drawing.Size(1774, 85);
             this.topPanel.TabIndex = 44;
             // 
             // findExistingVehicleLabel
@@ -167,42 +161,11 @@
             this.findExistingVehicleLabel.AutoSize = true;
             this.findExistingVehicleLabel.Font = new System.Drawing.Font("Segoe UI Emoji", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.findExistingVehicleLabel.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.findExistingVehicleLabel.Location = new System.Drawing.Point(32, 19);
-            this.findExistingVehicleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.findExistingVehicleLabel.Location = new System.Drawing.Point(43, 23);
             this.findExistingVehicleLabel.Name = "findExistingVehicleLabel";
-            this.findExistingVehicleLabel.Size = new System.Drawing.Size(173, 40);
+            this.findExistingVehicleLabel.Size = new System.Drawing.Size(220, 51);
             this.findExistingVehicleLabel.TabIndex = 0;
             this.findExistingVehicleLabel.Text = "Find branch";
-            // 
-            // updateButton
-            // 
-            this.updateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.updateButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.updateButton.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.updateButton.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.updateButton.Location = new System.Drawing.Point(383, 73);
-            this.updateButton.Margin = new System.Windows.Forms.Padding(2);
-            this.updateButton.Name = "updateButton";
-            this.updateButton.Size = new System.Drawing.Size(182, 30);
-            this.updateButton.TabIndex = 48;
-            this.updateButton.Text = "Update";
-            this.updateButton.UseVisualStyleBackColor = false;
-            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
-            // 
-            // deleteButton
-            // 
-            this.deleteButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.deleteButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.deleteButton.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.deleteButton.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.deleteButton.Location = new System.Drawing.Point(823, 73);
-            this.deleteButton.Margin = new System.Windows.Forms.Padding(2);
-            this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(182, 30);
-            this.deleteButton.TabIndex = 49;
-            this.deleteButton.Text = "Delete";
-            this.deleteButton.UseVisualStyleBackColor = false;
-            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
             // 
             // groupBox2
             // 
@@ -212,11 +175,11 @@
             this.groupBox2.Controls.Add(this.branchNameLabel);
             this.groupBox2.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.groupBox2.Location = new System.Drawing.Point(11, 226);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Location = new System.Drawing.Point(15, 278);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox2.Size = new System.Drawing.Size(345, 127);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(460, 156);
             this.groupBox2.TabIndex = 47;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Look up by Name";
@@ -229,35 +192,39 @@
             this.employeeIDGroupBox.Controls.Add(this.carIDLabel);
             this.employeeIDGroupBox.Font = new System.Drawing.Font("Segoe UI Emoji", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.employeeIDGroupBox.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.employeeIDGroupBox.Location = new System.Drawing.Point(11, 73);
-            this.employeeIDGroupBox.Margin = new System.Windows.Forms.Padding(2);
+            this.employeeIDGroupBox.Location = new System.Drawing.Point(15, 90);
+            this.employeeIDGroupBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.employeeIDGroupBox.Name = "employeeIDGroupBox";
-            this.employeeIDGroupBox.Padding = new System.Windows.Forms.Padding(2);
-            this.employeeIDGroupBox.Size = new System.Drawing.Size(345, 127);
+            this.employeeIDGroupBox.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.employeeIDGroupBox.Size = new System.Drawing.Size(460, 156);
             this.employeeIDGroupBox.TabIndex = 50;
             this.employeeIDGroupBox.TabStop = false;
             this.employeeIDGroupBox.Text = "Look up by Branch ID";
             // 
-            // userDataGridView
+            // branchInfoDataGridView
             // 
-            this.userDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.userDataGridView.Location = new System.Drawing.Point(383, 120);
-            this.userDataGridView.Name = "userDataGridView";
-            this.userDataGridView.Size = new System.Drawing.Size(622, 401);
-            this.userDataGridView.TabIndex = 46;
+            this.branchInfoDataGridView.AllowUserToAddRows = false;
+            this.branchInfoDataGridView.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.branchInfoDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.branchInfoDataGridView.Location = new System.Drawing.Point(495, 101);
+            this.branchInfoDataGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.branchInfoDataGridView.Name = "branchInfoDataGridView";
+            this.branchInfoDataGridView.Size = new System.Drawing.Size(1099, 724);
+            this.branchInfoDataGridView.TabIndex = 51;
+            this.branchInfoDataGridView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.branchInfoDataGridView_CellValueChanged);
+            this.branchInfoDataGridView.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.branchInfoDataGridView_EditingControlShowing);
             // 
             // FindBranch
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1084, 638);
+            this.ClientSize = new System.Drawing.Size(1774, 909);
+            this.Controls.Add(this.branchInfoDataGridView);
             this.Controls.Add(this.topPanel);
-            this.Controls.Add(this.updateButton);
-            this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.employeeIDGroupBox);
-            this.Controls.Add(this.userDataGridView);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FindBranch";
             this.Text = "FindBranch";
             this.topPanel.ResumeLayout(false);
@@ -266,7 +233,7 @@
             this.groupBox2.PerformLayout();
             this.employeeIDGroupBox.ResumeLayout(false);
             this.employeeIDGroupBox.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.userDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.branchInfoDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -283,10 +250,8 @@
         private System.Windows.Forms.Button branchIDSearchButton;
         private System.Windows.Forms.Panel topPanel;
         private System.Windows.Forms.Label findExistingVehicleLabel;
-        private System.Windows.Forms.Button updateButton;
-        private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox employeeIDGroupBox;
-        private System.Windows.Forms.DataGridView userDataGridView;
+        private System.Windows.Forms.DataGridView branchInfoDataGridView;
     }
 }
