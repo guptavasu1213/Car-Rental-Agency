14551ebdc1d27e1b4b22c346ef4cd11b0fd23ffd
Vasu Gupta <guptavasu1213@gmail.com>
2020-04-02 23:36:20 -0600

    Fix admin login

--- CarRentalApp/AdminLogin.cs
+++ CarRentalApp/AdminLogin.cs
@@ -42,7 +42,7 @@ namespace CarRentalApp
                 if (userPassHash == correctPassHash) { return true; }
                 else { MessageBox.Show("Incorrect Password!"); }
             }
-            return true; // ==================CHANGE LATER
+            return false; 
         }
 
 
